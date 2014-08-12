8f335c94a3a1e80a8aa713276c8772c473130268
Adam Vessey <adam@discoverygarden.ca>
2014-08-12 13:23:39 -0300

    Fix reference to script.

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ from distutils.core import setup
 setup(
   name='Resource Index/Solr Sync',
   version='1.0',
-  description='Ensure a Solr index is up-to-date with a Fedora Commons installation, based on the contents of the Resource Index.',
+  description='Sync Solr index by examining Resource Index.',
+  long_description='Ensure a Solr index is up-to-date with a Fedora Commons installation, based on the contents of the Resource Index.',
   author='discoverygarden Inc.',
   author_email='dev@discoverygarden.ca',
-  scripts=['bin/ri-solr-diff'],
+  scripts=['bin/ri-solr-diff.py'],
   requires=[
     'requests',
     'dateutil',
