046d97bf8c47ec81386e6656161baa70c5743d45
Adam Vessey <adam-vessey@users.noreply.github.com>
2018-03-09 15:01:14 -0400

    Alternate dryrun mechanism.
    Instead of making the updater more complex, just roll another class with
    the same interface.

--- bin/ri_solr_diff.py
+++ bin/ri_solr_diff.py
@@ -196,7 +196,7 @@ class solr_generator:
 class gsearch:
     """Helper class for prodding GSearch."""
 
-    def __init__(self, url, user, password, keep_docs, dryrun):
+    def __init__(self, url, user, password, keep_docs):
         """
         Constructor; stash state.
 
@@ -213,7 +213,6 @@ class gsearch:
         self.session.auth = (self.user, self.password)
         self.updated = False
         self.keep_docs = keep_docs
-        self.dryrun = dryrun
 
     def update_pid(self, pid):
         """Call to GSearch to update the given PID."""
@@ -225,20 +224,17 @@ class gsearch:
             'action': 'fromPid',
             'value': pid
         }
-        
-        if self.dryrun:
-            logging.info('Should update {0}...'.format(pid))
+
+        logging.debug('Attempting to update {0}...'.format(pid))
+        r = self.session.post(self.url, data=data)
+        if r.status_code == requests.codes.ok and not 'Object not found in low-level storage' in r.text:
+            logging.debug('Updated {0}'.format(pid))
+            logging.info(pid)
         else:
-            logging.debug('Attempting to update {0}...'.format(pid))
-            r = self.session.post(self.url, data=data)
-            if r.status_code == requests.codes.ok and not 'Object not found in low-level storage' in r.text:
-                logging.debug('Updated {0}'.format(pid))
-                logging.info(pid)
-            else:
-                reason = 'Not in Fedora' if r.status_code == requests.codes.ok and 'Object not found in low-level storage' in r.text else 'HTTP code {0}'.format(r.status_code)
-                logging.debug('Failed to update {0} ({1}).'.format(pid, reason))
-                if not self.keep_docs:
-                    self.delete_pid(pid)
+            reason = 'Not in Fedora' if r.status_code == requests.codes.ok and 'Object not found in low-level storage' in r.text else 'HTTP code {0}'.format(r.status_code)
+            logging.debug('Failed to update {0} ({1}).'.format(pid, reason))
+            if not self.keep_docs:
+                self.delete_pid(pid)
 
     def delete_pid(self, pid):
         """Call to GSearch to delete the given PID."""
@@ -250,16 +246,36 @@ class gsearch:
             'action': 'deletePid',
             'value': pid
         }
-        
-        if self.dryrun:
-            logging.info('Should delete {0}...'.format(pid))
+
+        logging.debug('Attempting to delete {0}...'.format(pid))
+        r = self.session.post(self.url, data=data)
+        if r.status_code == requests.codes.ok:
+            logging.debug('Deleted {0}'.format(pid))
         else:
-            logging.debug('Attempting to delete {0}...'.format(pid))
-            r = self.session.post(self.url, data=data)
-            if r.status_code == requests.codes.ok:
-                logging.debug('Deleted {0}'.format(pid))
-            else:
-                logging.debug('Failed to delete {0} (HTTP code {1}).'.format(pid, r.status_code))
+            logging.debug('Failed to delete {0} (HTTP code {1}).'.format(pid, r.status_code))
+
+class gsearch_dryrun:
+    """Helper class to represent GSearch API actions."""
+
+    def __init__(self):
+        """
+        Constructor; stash state.
+        """
+        self.updated = False
+
+    def update_pid(self, pid):
+        """Log action of for actual run with the given PID."""
+        if not self.updated:
+            self.updated = True
+
+        logging.info('Should update {0}'.format(pid))
+
+    def delete_pid(self, pid):
+        """Call to GSearch to delete the given PID."""
+        if not self.updated:
+            self.updated = True
+
+        logging.info('Should delete {0}'.format(pid))
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -296,7 +312,7 @@ if __name__ == '__main__':
 
     ri = iter(ri_generator(args.ri, args.ri_user, args.ri_pass, start=start, limit=args.query_limit))
     solr = iter(solr_generator(args.solr, args.solr_last_modified_field, start=start, limit=args.query_limit))
-    gsearch = gsearch(args.gsearch, args.gsearch_user, args.gsearch_pass, args.keep_docs, args.dryrun)
+    gsearch = gsearch_dryrun() if args.dryrun else gsearch(args.gsearch, args.gsearch_user, args.gsearch_pass, args.keep_docs)
 
     try:
         ri_result = ri.next()
